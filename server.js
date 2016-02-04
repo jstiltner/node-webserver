9d8c58f264f6847c92bd7207daa606c494d2930c
Jason Stiltner <jasonstiltner@gmail.com>
2016-02-04 12:59:06 -0600

    changing the uploaded file name and destination

--- server.js
+++ server.js
@@ -2,11 +2,13 @@
 
 const app = require('express')();
 const bodyParser = require('body-parser');
+const upload = require('multer')({ dest: 'tmp/uploads' });;
+const fs = require('fs');
 const PORT = process.env.PORT || 3000
 // tells application dependencies to transpile Jade to HTML
 app.set('view engine', 'jade');
 
-app.use(bodyParser.urlencoded({ extended: false}));
+//app.use(bodyParser.urlencoded({ extended: false}));
 
 app.get('/',(req, res) => {
   setTimeout(() => {
@@ -28,8 +30,37 @@ app.post('/contact', (req,res) => {
   res.send(`<h1>Thanks for contacting us, ${name}</h1>`);
 });
 
+app.get('/send-photo', (req,res) => {
+  res.render('sendphoto');
+});
+
+app.post('/send-photo', upload.single('image'), (req,res) => {
+  console.log(req.body, req.file);
+  //res.send('<h1>Thanks for submitting your photos</h1>');
+  // get the temporary location of the file
+  const tmp_path = req.file.path;
+  //get the extention of the file
+  const fullName = req.file.originalname;
+  const splitRes = fullName.split(".");
+  const newFileName = req.file.filename + '.' + splitRes[1];
+  console.log(newFileName);
+  // set where the file should actually exists - in this case it is in the "images" directory
+  const target_path = 'tmp/renamed/' + newFileName;
+  // move the file from the temporary location to the intended location
+  fs.rename(tmp_path, target_path, function(err) {
+      if (err) throw err;
+        // delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
+      fs.unlink(tmp_path, function() {
+          if (err) throw err;
+          res.send('File uploaded to: ' + target_path + ' - ' + req.file.size + ' bytes');
+        });
+  });
+});
+
+
 app.get('/hello',(req,res) => {
   const name = req.query.name;
+  console.log ('req.query');
   const msg = `<h1>Hello ${name}!</h1><h2>Goodbye ${name}!</h2>`;
 
     console.log('QUERY PARAMS>>>>>', req.query);
@@ -59,9 +90,6 @@ function getRandomInt(min, max) {
     const month = require('node-cal/lib/month');
     console.log(month);
   });
-
-
-
 
   app.get('/random/:min/:max',(req,res)=>{
   const min = req.params.min;
